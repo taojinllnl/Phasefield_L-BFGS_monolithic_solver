4c13abe165d2fef13588b018c3601833efe6f1ec
Tao Jin <taojin83@gmail.com>
2026-08-20 16:18:05 -0400

    Update input files for simple tension test (monotonical loading)

--- simple_tension_test/plot_reaction_force.py
+++ simple_tension_test/plot_reaction_force.py
@@ -23,35 +23,8 @@ symbols = ['-k', '--*m', '-.b^', ':gx', '-rD', 'bx']
 fig = plt.figure()
 ax = fig.add_subplot(111)
 
-plt.plot(time1[:25], forceY1[:25], symbols[0],
-         linewidth=2.0, label=labels[0], fillstyle='none', markersize=8)
-plt.plot(time1[25:], forceY1[25:], symbols[1],
-         linewidth=2.0, label=labels[1], fillstyle='none', markersize=8)  
-#plt.plot(time2, forceX2, symbols[1],
-#         linewidth=2.0, label=labels[1], fillstyle='none', markersize=8)   
-# plt.ticklabel_format(axis='y', style='sci', scilimits=(0, 3))
-# ax.yaxis.set_major_formatter(mtick.FormatStrFormatter('%.1e'))
-
-plt.xlabel('Pseudo time')
-plt.ylabel('Reaction force (kN)', multialignment='center')
-#plt.xlim(0,0.05)
-# plt.xticks(np.arange(0, 6, step=1))
-#plt.ylim(0.0, 1.0)
-# plt.legend( bbox_to_anchor=(2.4, 1.1),prop={'size':10})
-plt.grid()
-plt.legend()
-# ax.set_aspect('equal')
-# plt.subplots_adjust(top = 0.95, bottom = 0.125,
-#                     right = 0.95, left = 0.125, hspace = 0, wspace = 0)
-plt.savefig("Reaction_force_history_time.eps", bbox_inches='tight', pad_inches=0.1)
-
-fig = plt.figure()
-ax = fig.add_subplot(111)
-
-plt.plot(time1[:25], forceY1[:25], symbols[0],
-         linewidth=2.0, label=labels[0], fillstyle='none', markersize=8)
-plt.plot(14.0e-3 - time1[25:], forceY1[25:], symbols[1],
-         linewidth=2.0, label=labels[1], fillstyle='none', markersize=8)              
+plt.plot(time1, forceY1, symbols[0],
+         linewidth=2.0, label=labels[0], fillstyle='none', markersize=8)          
 #plt.plot(time2, forceX2, symbols[1],
 #         linewidth=2.0, label=labels[1], fillstyle='none', markersize=8)   
 # plt.ticklabel_format(axis='y', style='sci', scilimits=(0, 3))
